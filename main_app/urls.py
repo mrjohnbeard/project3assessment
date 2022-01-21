4f17510290ff2222c2da8b6d4f424a3f2d054d31
mrjohnbeard <wengerbeard@gmail.com>
2022-01-21 13:49:55 -0800

    AssessMent Complete

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
-from main_app.models import Widget
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('addwidget/', views.add_widget,
+         name='add_widget'),
+     path('removewidget/<int:widget_id>/', views.remove_widget, name='remove_widget')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Widget
-
+from .forms import WidgetForm
 # Create your views here.
 
 def home(request):
   widgets = Widget.objects.all()
-  return render (request, 'home.html', {' widgets': widgets})
+  form = WidgetForm()
+  return render (request, 'home.html', {'widgets': widgets, 'form': form })
+
+def add_widget(request):
+    widget = WidgetForm(request.POST)
+    if widget.is_valid():
+        widget.save()
+        return redirect('home')
+    return redirect('home') 
+  
+def remove_widget(request, widget_id):
+    widget = Widget.objects.get(id=widget_id)
+    widget.delete()
+    return redirect('home')
